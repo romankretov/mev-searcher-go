b867340c2c5029460cb99527f376a4b575d7079f
Roman Kretov <roman.kretov.02@gmail.com>
2026-07-20 23:35:59 +0100

    added a pooltarget slice

--- main.go
+++ main.go
@@ -8,12 +8,30 @@ import (
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"log"
+	"math/big"
 	"searcher-bot/contracts/uniswapv2pair"
+	"sync"
+	"time"
 )
 
 type PoolTarget struct {
-	Name    string
-	Address common.Address
+	Name     string
+	Address  common.Address
+	Contract *uniswapv2pair.UniswapV2Pair
+	Token0   common.Address
+	Token1   common.Address
+}
+
+type PoolState struct {
+	Name               string
+	Reserve0, Reserve1 *big.Int
+	BlockNumber        uint64
+	UpdatedAt          time.Time
+}
+
+type StateCache struct {
+	mu    sync.RWMutex
+	pools map[common.Address]PoolState
 }
 
 func main() {
@@ -33,11 +51,33 @@ func main() {
 	}
 	defer sub.Unsubscribe()
 
-	pairAddress := common.HexToAddress("0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc")
+	pools := []PoolTarget{
+		{Name: "Uniswap V2 USDC/WETH", Address: common.HexToAddress("0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc")},
+		{Name: "Sushiswap USDC/WETH", Address: common.HexToAddress("0x397FF1542f962076d0BFE58eA045FfA2d347ACa0")},
+	}
 
-	pair, err := uniswapv2pair.NewUniswapV2Pair(pairAddress, client)
-	if err != nil {
-		log.Fatalf("Error creating a pair %v", err)
+	for i := range pools {
+		contract, err := uniswapv2pair.NewUniswapV2Pair(pools[i].Address, client)
+		if err != nil {
+			log.Fatal("error binding a contract")
+		}
+
+		pools[i].Contract = contract
+		callOpts := &bind.CallOpts{
+			Context: context.Background(),
+		}
+		token0, err := contract.Token0(callOpts)
+		if err != nil {
+			log.Fatal("error fetching token0")
+		}
+		pools[i].Token0 = token0
+
+		token1, err := contract.Token1(callOpts)
+		if err != nil {
+			log.Fatal("error fetching token1")
+		}
+		pools[i].Token1 = token1
+		fmt.Printf("Bound %s at %s (token0 = %s   token1 = %s) \n", pools[i].Name, pools[i].Address.Hex(), pools[i].Token0.Hex(), pools[i].Token1.Hex())
 	}
 
 	fmt.Println("Listenning to new blocks....")
@@ -53,19 +93,6 @@ func main() {
 				log.Println("Invalid header received")
 				continue
 			}
-			callOpts := &bind.CallOpts{
-				BlockNumber: header.Number,
-				Context:     ctx,
-			}
-			reserves, err := pair.GetReserves(callOpts)
-			if err != nil {
-				log.Printf("Error fetching new blocks pair reserves: %v", err)
-				continue
-			}
-			fmt.Printf(
-				"block=%v, reserve0=%v, reserve1=%v, timestamp=%v \n",
-				header.Number.String(), reserves.Reserve0.String(), reserves.Reserve1.String(), reserves.BlockTimestampLast,
-			)
 		case err := <-sub.Err():
 			log.Fatalf("Subscribption error %v", err)
 
