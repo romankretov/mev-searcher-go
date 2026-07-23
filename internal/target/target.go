616ca5e900d895cebf766e5197755cde191f27c9
Roman Kretov <roman.kretov.02@gmail.com>
2026-07-23 15:27:43 +0100

    refactor for target pool, separate package

---
+++ internal/target/target.go
@@ -0,0 +1,56 @@
+package target
+
+import (
+	"context"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"searcher-bot/contracts/uniswapv2pair"
+	"searcher-bot/internal/cache"
+)
+
+type PoolTarget struct {
+	Name     string
+	Address  common.Address
+	Contract *uniswapv2pair.UniswapV2Pair
+	Token0   common.Address
+	Token1   common.Address
+}
+
+func LoadTargets(ctx context.Context, client *ethclient.Client, targets map[string]string, decimalsCache *cache.DecimalsCache) []PoolTarget {
+	poolTargets := []PoolTarget{}
+	for k, v := range targets {
+		contract, err := uniswapv2pair.NewUniswapV2Pair(common.HexToAddress(k), client)
+		if err != nil {
+			continue
+		}
+		target := PoolTarget{
+			Name:     k,
+			Address:  common.HexToAddress(v),
+			Contract: contract,
+		}
+
+		callOpts := &bind.CallOpts{
+			Context: ctx,
+		}
+		token0, err := contract.Token0(callOpts)
+		if err != nil {
+			continue
+		}
+		token1, err := contract.Token1(callOpts)
+		if err != nil {
+			continue
+		}
+		target.Token0 = token0
+		target.Token1 = token1
+		poolTargets = append(poolTargets, target)
+		if err := decimalsCache.Fetch(target.Token0, client, ctx); err != nil {
+			continue
+		}
+		if err := decimalsCache.Fetch(target.Token1, client, ctx); err != nil {
+			continue
+		}
+
+	}
+	return poolTargets
+}
--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"log"
 	"searcher-bot/contracts/uniswapv2pair"
 	"searcher-bot/internal/cache"
+	"searcher-bot/internal/target"
 	"time"
 )
 
@@ -40,40 +41,11 @@ func main() {
 	}
 	defer sub.Unsubscribe()
 
-	pools := []PoolTarget{
-		{Name: "Uniswap V2 USDC/WETH", Address: common.HexToAddress("0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc")},
-		{Name: "Sushiswap USDC/WETH", Address: common.HexToAddress("0x397FF1542f962076d0BFE58eA045FfA2d347ACa0")},
+	pools := map[string]string{
+		"Uniswap V2 USDC/WETH": "0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc",
+		"Sushiswap USDC/WETH":  "0x397FF1542f962076d0BFE58eA045FfA2d347ACa0",
 	}
-
-	for i := range pools {
-		contract, err := uniswapv2pair.NewUniswapV2Pair(pools[i].Address, client)
-		if err != nil {
-			log.Fatal("error binding a contract")
-		}
-
-		pools[i].Contract = contract
-		callOpts := &bind.CallOpts{
-			Context: context.Background(),
-		}
-		token0, err := contract.Token0(callOpts)
-		if err != nil {
-			log.Fatal("error fetching token0")
-		}
-		pools[i].Token0 = token0
-
-		token1, err := contract.Token1(callOpts)
-		if err != nil {
-			log.Fatal("error fetching token1")
-		}
-		pools[i].Token1 = token1
-		fmt.Printf("Bound %s at %s (token0 = %s   token1 = %s) \n", pools[i].Name, pools[i].Address.Hex(), pools[i].Token0.Hex(), pools[i].Token1.Hex())
-		if err := decimalsCache.Fetch(token0, client, ctx); err != nil {
-			log.Fatal("error fetching decimals")
-		}
-		if err := decimalsCache.Fetch(token1, client, ctx); err != nil {
-			log.Fatal("error fetching decimals")
-		}
-	}
+	targetPool := target.LoadTargets(ctx, client, pools, decimalsCache)
 
 	fmt.Println("Listenning to new blocks....")
 
@@ -88,7 +60,7 @@ func main() {
 				log.Println("Invalid header received")
 				continue
 			}
-			for _, pool := range pools {
+			for _, pool := range targetPool {
 				callCtx, cancel := context.WithTimeout(context.Background(), time.Second*5)
 				callOpts := &bind.CallOpts{
 					Context:     callCtx,
@@ -110,7 +82,7 @@ func main() {
 					},
 				)
 			}
-			for _, pool := range pools {
+			for _, pool := range targetPool {
 				state, ok := stateCache.Get(pool.Address)
 				if !ok {
 					log.Fatal("error fetching state from cache")
